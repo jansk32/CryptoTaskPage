82b3d0c9189c607b4431bc2d84a6194bc12cae3b
jansk32 <44828185+jansk32@users.noreply.github.com>
2021-10-11 15:36:08 +1100

    update loading screen

--- cryptoinfo/src/loadingComponent.js
+++ cryptoinfo/src/loadingComponent.js
@@ -2,7 +2,7 @@ import React from 'react'
 import load from './loading_logo.png'
 function LoadingComponent() {
     return <div>
-        <center>
+        <center style={{top: "30%"}}>
         <img className="App-logo" src={load} alt="Loading..." style={{width: "20%", height: "20%"}}/>
         <h1>Loading...</h1>
         </center>
